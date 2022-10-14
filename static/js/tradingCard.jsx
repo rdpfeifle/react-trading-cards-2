c1e18a638313ea3118b87785c31f5adcebca9b6a
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2022-10-14 17:48:27 -0600

    Completed Further Study

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,54 +1,3 @@
-const tradingCardData = [
-  {
-    name: "Balloonicorn",
-    skill: "video games",
-    imgUrl: "/static/img/balloonicorn.jpg",
-    cardId: 1,
-  },
-  {
-    name: "Float",
-    skill: "baking pretzels",
-    imgUrl: "/static/img/float.jpg",
-    cardId: 2,
-  },
-  {
-    name: "Llambda",
-    skill: "knitting scarves",
-    imgUrl: "/static/img/llambda.jpg",
-    cardId: 3,
-  },
-  {
-    name: "Off-By-One",
-    skill: "climbing mountains",
-    imgUrl: "/static/img/off-by-one.jpeg",
-    cardId: 4,
-  },
-  {
-    name: "Seed.py",
-    skill: "making curry dishes",
-    imgUrl: "/static/img/seedpy.jpeg",
-    cardId: 5,
-  },
-  {
-    name: "Polymorphism",
-    skill: "costumes",
-    imgUrl: "/static/img/polymorphism.jpeg",
-    cardId: 6,
-  },
-  {
-    name: "Short Stack Overflow",
-    skill: "ocean animal trivia",
-    imgUrl: "/static/img/shortstack-overflow.jpeg",
-    cardId: 7,
-  },
-  {
-    name: "Merge",
-    skill: "bullet journaling",
-    imgUrl: "/static/img/merge.png",
-    cardId: 8,
-  },
-];
-
 function TradingCard(props) {
   return (
     <div className="card">
@@ -60,13 +9,12 @@ function TradingCard(props) {
 }
 
 function TradingCardContainer() {
-  // const floatCard = {
-  //   name: "Float",
-  //   skill: "baking pretzels",
-  //   imgUrl: "/static/img/float.jpg",
-  // };
-
   const [cards, setCards] = React.useState([]);
+
+  function addCard(newCard) {
+    const currentCards = [...cards];
+    setCards([...currentCards, newCard]);
+  }
 
   React.useEffect(() => {
     fetch("/cards.json")
@@ -86,18 +34,61 @@ function TradingCardContainer() {
       />
     );
   }
-  // for (const currentCard of tradingCardData) {
-  //   tradingCards.push(
-  //     <TradingCard
-  //       key={currentCard.cardId}
-  //       name={currentCard.name}
-  //       skill={currentCard.skill}
-  //       imgUrl={currentCard.imgUrl}
-  //     />,
-  //   );
-  // }
 
-  return <div className="grid">{tradingCards}</div>;
+  return (
+    <React.Fragment>
+      <AddTradingCard />
+      <h2>Trading Cards</h2>
+      <div className="grid">{tradingCards}</div>
+    </React.Fragment>
+  );
 }
-
 ReactDOM.render(<TradingCardContainer />, document.getElementById("container"));
+
+function AddTradingCard() {
+  const [name, setName] = React.useState("");
+  const [skill, setSkill] = React.useState("");
+
+  function addNewCard() {
+    fetch("/add-card", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // this could also be written as body: JSON.stringify({ name, skill }) with
+      // JS object property value shorthand
+      body: JSON.stringify({ name: name, skill: skill }),
+    })
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        // use AJAX here to "addCard"
+        //alert(`Card added! Response: ${jsonResponse}`);
+      });
+  }
+  return (
+    <React.Fragment>
+      <h2>Add New Trading Card</h2>
+      <label htmlFor="nameInput">Name</label>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="nameInput"
+        style={{ marginLeft: "5px" }}
+      ></input>
+      <label
+        htmlFor="skillInput"
+        style={{ marginLeft: "10px", marginRight: "5px" }}
+      >
+        Skill
+      </label>
+      <input
+        value={skill}
+        onChange={(event) => setSkill(event.target.value)}
+        id="skillInput"
+      ></input>
+      <button style={{ marginLeft: "10px" }} onClick={addNewCard}>
+        Add
+      </button>
+    </React.Fragment>
+  );
+}
